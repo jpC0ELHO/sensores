3a799f291f27af47eb77424ad6a6889bae27e33e
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-07 16:35:13 -0300

    created DeteccaoTipo

---
+++ src/main/java/smarthouse/Sensores/domain/entities/enums/DeteccaoTipo.java
@@ -0,0 +1,32 @@
+package smarthouse.Sensores.domain.entities.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum DeteccaoTipo {
+    OBJETO("Objeto"),
+    OBJETO_MOVIMENTO("Objeto em Movimento"),
+    ANIMAL("Animal"),
+    ANIMAL_MOVIMENTO("Animal em Movimento"),
+    CALOR_ALTO("Calor Alto"),
+    HIPERCALOR("Hipercalor");
+
+    private final String descricao;
+
+    DeteccaoTipo(String descricao){
+        this.descricao=descricao;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static DeteccaoTipo fromDescricao(String descricao) {
+        for (DeteccaoTipo tipo : DeteccaoTipo.values()) {
+            if (tipo.getDescricao().equalsIgnoreCase(descricao)) {
+                return tipo;
+            }
+        }
+        throw new IllegalArgumentException("Tipo de deteccão inválido: " + descricao);
+    }
+}
+
