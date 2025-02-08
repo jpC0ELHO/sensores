b786590a63b4401b09eeb62e66c0c975b3224b91
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-08 16:53:14 -0300

    update Eventos.java

--- src/main/java/smarthouse/Sensores/domain/entities/enums/Eventos.java
+++ src/main/java/smarthouse/Sensores/domain/entities/enums/Eventos.java
@@ -1,4 +1,26 @@
 package smarthouse.Sensores.domain.entities.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Eventos {
+    //brevidades
+    INCENDIO("INCENDIO"),
+    COMBUSTAO("COMBUSTAO"),
+    VAZAMENTO_GAS("VAZAMENTO GAS"),
+    //Atipicos
+    FALHA("FALHA"),
+    LIQUIDO("LIQUIDO"),
+    AMEACA("AMEACA"),
+    //sensor de calor
+    PRESENCA("PRESENCA"),
+    //cameras de seguranca
+    SUSPEITO("SUSPEITO"),
+    CONHECIDO("CONEHCIDO");
+
+    private final String descricao;
+
+    Eventos(String descricao){
+        this.descricao=descricao;
+    }
 }
