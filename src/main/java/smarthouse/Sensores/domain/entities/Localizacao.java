6d6052e44de6c30482928ad224f53b408b0e9946
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-08 15:53:06 -0300

    created Localizacao.java

---
+++ src/main/java/smarthouse/Sensores/domain/entities/Localizacao.java
@@ -0,0 +1,32 @@
+package smarthouse.Sensores.domain.entities;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import smarthouse.Sensores.domain.entities.enums.Regiao;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "localizacao")
+@EntityListeners(AuditingEntityListener.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Localizacao extends Entidade{
+
+    @ManyToOne
+    @JoinColumn(name = "sensor_id", nullable = false)
+    private Sensor sensor;
+    @Column(precision = 5,scale = 2,name = "Longitude",nullable = false)
+    private Double latitude;
+    @Column(precision = 5,scale = 2,name = "Longitude",nullable = false)
+    private Double longitude;
+
+    @Enumerated(EnumType.STRING)
+    private Regiao localidade;
+
+}
