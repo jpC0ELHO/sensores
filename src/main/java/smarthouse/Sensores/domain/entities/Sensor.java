68afa083e10bc8b2c46fff0cd296f6e7b9307252
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-13 13:20:55 -0300

    update Sensor.java

--- src/main/java/smarthouse/Sensores/domain/entities/Sensor.java
+++ src/main/java/smarthouse/Sensores/domain/entities/Sensor.java
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
-import com.fasterxml.jackson.databind.ser.std.DateTimeSerializerBase;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +16,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import smarthouse.Sensores.domain.entities.enums.SensorTipo;
 import smarthouse.Sensores.domain.entities.enums.UnidadeMemoria;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
@@ -31,20 +32,21 @@ public class Sensor extends Entidade{
     @Column(name = "localizacao_sensor")
     private Localizacao localizacao;
     @Column(name = "tipo_sensor")
+    @Enumerated(EnumType.STRING)
     private SensorTipo sensorTipo;
     @Column(name = "status")
-    private Boolean ativadoDesativado;
+    private boolean ativadoDesativado;
     @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
-    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
-    @JsonSerialize(using = DateTimeSerializerBase.class)
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalDateTime horarioAcionamento;
     @Column(name = "memoria_usada", precision = 15, scale = 2)
-    private Long memoriaUsada;
+    private BigDecimal memoriaUsada;
     @Column(name = "memoria_disponivel", precision = 15, scale = 2)
-    private Long memoriaDisponivel;
+    private BigDecimal memoriaDisponivel;
     @Column(name = "valor_dados_transferencia", precision = 15, scale = 2)
-    private Long valorDadosTransferencia;
+    private BigDecimal valorDadosTransferencia;
     @Column(name = "decricao_dados")
     private String dadosDescricao;
     @Enumerated(EnumType.STRING)
