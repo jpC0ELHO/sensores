e7b753e33959d3a539b66814cf5f23e1db97b550
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-13 13:27:08 -0300

    created LeituraRepository.java

---
+++ src/main/java/smarthouse/Sensores/domain/repositories/LeituraRepository.java
@@ -0,0 +1,11 @@
+package smarthouse.Sensores.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import smarthouse.Sensores.domain.entities.Leitura;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface LeituraRepository extends JpaRepository<Leitura, UUID> {
+    Optional<Leitura>findById(UUID uuid);
+}
