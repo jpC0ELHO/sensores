3c46315737885c99f1e2feaaba2f107cc7c56220
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-10 10:39:57 -0300

    created SensorResponse

---
+++ src/main/java/smarthouse/Sensores/api/dtos/SensorResponse.java
@@ -0,0 +1,53 @@
+package smarthouse.Sensores.api.dtos;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import smarthouse.Sensores.domain.entities.Localizacao;
+import smarthouse.Sensores.domain.entities.Sensor;
+import smarthouse.Sensores.domain.entities.enums.ProtocoloComunicacao;
+import smarthouse.Sensores.domain.entities.enums.SensorTipo;
+import smarthouse.Sensores.domain.entities.enums.UnidadeMemoria;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@JsonPropertyOrder({"name","localizacao","horarioAcionamento","memoriaUsada","memoriaDisponivel"
+        ,"valorDadosTransferencia","descricao","unidadeMemoria","protocoloComunicacao","createdBy","lastModifiedBy"
+,"createdAt","updateAt"})
+public record SensorResponse (
+        String name,
+        Localizacao localizacao,
+        SensorTipo sensorTipo,
+        LocalDateTime horarioAcionamento,
+        BigDecimal memoriaUsada,
+        BigDecimal memoriaDisponivel,
+        BigDecimal valorDadosTransferencia,
+        String descricao,
+        UnidadeMemoria unidadeMemoria,
+        ProtocoloComunicacao protocoloComunicacao,
+        String createdBy,
+        String lastModifiedBy,
+        LocalDateTime createdAt,
+        LocalDateTime updateAt
+){
+    public static SensorResponse toResponse(Sensor sensor){
+        if (sensor==null){
+            throw new NullPointerException("Null");
+        }
+        return new SensorResponse(
+                sensor.getName(),
+                sensor.getLocalizacao(),
+                sensor.getSensorTipo(),
+                sensor.getHorarioAcionamento(),
+                sensor.getMemoriaUsada(),
+                sensor.getMemoriaDisponivel(),
+                sensor.getValorDadosTransferencia(),
+                sensor.getDadosDescricao(),
+                sensor.getUnidadeMemoria(),
+                sensor.getProtocoloComunicacao(),
+                sensor.getCreatedBy(),
+                sensor.getLastModifiedBy(),
+                sensor.getCreatedAt(),
+                sensor.getUpdateAt()
+        );
+    }
+}
