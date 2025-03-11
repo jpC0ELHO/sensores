132ce7abea5828a70a2a9146c813f7c80af8bcad
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-11 15:53:39 -0300

    update SensorResponse.java

--- src/main/java/smarthouse/Sensores/api/dtos/SensorResponse.java
+++ src/main/java/smarthouse/Sensores/api/dtos/SensorResponse.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import smarthouse.Sensores.domain.entities.Localizacao;
 import smarthouse.Sensores.domain.entities.Sensor;
 import smarthouse.Sensores.domain.entities.enums.ProtocoloComunicacao;
+import smarthouse.Sensores.domain.entities.enums.Regiao;
 import smarthouse.Sensores.domain.entities.enums.SensorTipo;
 import smarthouse.Sensores.domain.entities.enums.UnidadeMemoria;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@JsonPropertyOrder({"name","localizacao","sensorTipo","ativadoDesativado",
+@JsonPropertyOrder({"name","localizacao","sensorTipo","regiao","ativadoDesativado",
         "horarioAcionamento","memoriaUsada","memoriaDisponivel"
         ,"valorDadosTransferencia","descricao","unidadeMemoria",
         "protocoloComunicacao","createdBy","lastModifiedBy"
@@ -19,6 +20,7 @@ public record SensorResponse (
         String name,
         Localizacao localizacao,
         SensorTipo sensorTipo,
+        Regiao regiao,
         boolean ativadoDesativado,
         LocalDateTime horarioAcionamento,
         BigDecimal memoriaUsada,
@@ -40,6 +42,7 @@ public record SensorResponse (
                 sensor.getName(),
                 sensor.getLocalizacao(),
                 sensor.getSensorTipo(),
+                sensor.getRegiao(),
                 sensor.isAtivadoDesativado(),
                 sensor.getHorarioAcionamento(),
                 sensor.getMemoriaUsada(),
