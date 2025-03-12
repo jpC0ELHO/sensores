756aef9988a399dfcb2d3460c4abc10cf037a0cb
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-12 14:34:13 -0300

    update LeituraRespository

--- src/main/java/smarthouse/Sensores/domain/repositories/LeituraRespository.java
+++ src/main/java/smarthouse/Sensores/domain/repositories/LeituraRespository.java
@@ -2,10 +2,14 @@ package smarthouse.Sensores.domain.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import smarthouse.Sensores.domain.entities.Leitura;
+import smarthouse.Sensores.domain.entities.enums.DeteccaoTipo;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface LeituraRespository extends JpaRepository<Leitura, UUID> {
     Optional<Leitura>findById(UUID uuid);
+    Optional<Leitura>findLeituraByTipo(DeteccaoTipo deteccaoTipo);
+    Optional<Leitura>findLeituraByHorario(LocalDateTime horarioDetec);
 }
