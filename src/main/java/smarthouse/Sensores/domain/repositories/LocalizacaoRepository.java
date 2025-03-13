c29565dff3c9f6e8594f5867117689a453eb8ff9
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-13 14:38:46 -0300

    update LocalizacaoRepository

--- src/main/java/smarthouse/Sensores/domain/repositories/LocalizacaoRepository.java
+++ src/main/java/smarthouse/Sensores/domain/repositories/LocalizacaoRepository.java
@@ -2,10 +2,14 @@ package smarthouse.Sensores.domain.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import smarthouse.Sensores.domain.entities.Localizacao;
+import smarthouse.Sensores.domain.entities.Sensor;
+import smarthouse.Sensores.domain.entities.enums.Regiao;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface LocalizacaoRepository extends JpaRepository<Localizacao, UUID> {
     Optional<Localizacao>findById(UUID uuid);
+    Optional<Localizacao>findByLatitude(Double latitude);
+    Optional<Localizacao>findByLongitude(Double longitude);
 }
