c3dbc83c928545b48887ac9845220d93df0777f7
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-13 14:38:29 -0300

    update Localizacao.java

--- src/main/java/smarthouse/Sensores/domain/entities/Localizacao.java
+++ src/main/java/smarthouse/Sensores/domain/entities/Localizacao.java
@@ -1,13 +1,22 @@
 package smarthouse.Sensores.domain.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+import smarthouse.Sensores.domain.entities.enums.ProtocoloComunicacao;
 import smarthouse.Sensores.domain.entities.enums.Regiao;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -25,6 +34,14 @@ public class Localizacao extends Entidade{
     @Column(precision = 5,scale = 2,name = "Longitude",nullable = false)
     private Double longitude;
     @Enumerated(EnumType.STRING)
-    private Regiao localidade;
+    private Regiao regiao;
+    @Enumerated(EnumType.STRING)
+    private ProtocoloComunicacao protocoloComunicacao;
+    @Column(name = "data_instalacao")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd/MM/yyyy HH:mm:ss")
+    @JsonSerialize(using = DateSerializer.class)
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
+    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime dataInstalacao;
 
 }
